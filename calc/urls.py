764c6ef8a64c780b6b65cf22fe6a2f37ad02a0bd
prakashcool12 <dhungana.nara@gmail.com>
2019-08-28 15:18:00 +0545

    adding two numbers

--- calc/urls.py
+++ calc/urls.py
@@ -3,8 +3,9 @@ from django.urls import path
 from .import views 
 
 urlpatterns=[
-    path('',views.home,name="home")
-
+    path('',views.home,name="home"),
+    path('add',views.add, name='add')
+  
 
 
 ]
--- calc/views.py
+++ calc/views.py
@@ -4,4 +4,12 @@ from django.http import  HttpResponse
 # Create your views here.
 
 def home(request):
-    return render(request,'home.html',{'name':'Life'})
+    return render(request,'home.html',{'name':'Narayan'})
+
+def add(request):
+
+    val1=request.GET['num1']
+    val1=request.GET['num2']
+    res=val1+val2
+
+    return render(request,'result.html',{'result':res})
